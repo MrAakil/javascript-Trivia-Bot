22551c69d3f023069f4ff24ce0d8deb9d9091995
Mohammed Aakil <aakilmohammed213@gmail.com>
2026-03-28 15:15:14 +0530

    Add trivia bot with coding fun facts

---
+++ trivia.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Aakil";
+let botLocation = "Puttalam";
+let favoriteLanguage = "javascript";
+
+console.log("My name is "+ botName + " and I live on "+ botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage+ ".");
+
+
+let codingFact = "The fun fact about "+ favoriteLanguage + ", It is a programming language.";
+console.log(codingFact);
+
+codingFact = "The new fact about" + favoriteLanguage + ", It dynamically changes the website behaviour.";
+console.log(codingFact);
+
+codingFact = "The another new fact about" + favoriteLanguage + ", It is used for mostly to HTML and CSS.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".")
